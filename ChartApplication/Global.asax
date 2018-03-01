e50d6ff690b751a14b62e09b6320cd9a4dc80b37
jmlindsey <jonathon.m.lindsey@gmail.com>
2018-03-01 14:04:27 -0600

    Add project files.

---
+++ ChartApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChartApplication.MvcApplication" Language="C#" %>
---
+++ ChartApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChartApplication.Startup))]
+namespace ChartApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
